d802d912e23e886282a6625a93a2c2065b32ccd0
Juan Guerrero <JGuerrer@IGJ206444>
2023-11-08 15:59:58 -0300

    add files js validation

---
+++ Balances.WebAssembly/wwwroot/js/getEntidad.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    // Recuperar los valores de sessionStorage
+    var razonSocial = sessionStorage.getItem('RazonSocial');
+    var tipoEntidad = sessionStorage.getItem('TipoEntidad');
+
+    // Verificar si los valores existen en sessionStorage
+    if (razonSocial && tipoEntidad) {
+        // Actualizar los elementos HTML con los valores recuperados
+        document.getElementById('razonSocial').textContent = razonSocial;
+        document.getElementById('tipoEntidad').textContent = tipoEntidad;
+    }
+});
