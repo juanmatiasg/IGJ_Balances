d5ae3e649304f031ae94b864de88a9f97963dc61
alesl10 <lopezalexis499@gmail.com>
2024-05-23 15:06:24 -0300

    actualizando diseño autoridades, falta validar CUIT/DNI

--- Balances.DTO/AutoridadDto.cs
+++ Balances.DTO/AutoridadDto.cs
@@ -16,6 +16,7 @@
         public string Cargo { get; set; }
 
         public bool EsFirmante { get; set; }
+        public bool EstaVigente { get; set; }
 
         //public BalanceDto Balance { get; set; }
     }
