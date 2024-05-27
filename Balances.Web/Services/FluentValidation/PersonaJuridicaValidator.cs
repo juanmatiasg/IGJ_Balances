be8b3db4fd3574d959f5897f985e08fda5ef4ff8
Juan Guerrero <juanmatiasg@hotmail.com>
2024-05-27 12:41:59 -0300

    nuevos cambios integrantes

--- Balances.Web/Services/FluentValidation/PersonaJuridicaValidator.cs
+++ Balances.Web/Services/FluentValidation/PersonaJuridicaValidator.cs
@@ -8,16 +8,53 @@ namespace Balances.Web.Services.FluentValidation
         public PersonaJuridicaValidator()
         {
             RuleFor(_ => _.Denominacion).Cascade(CascadeMode.Stop)
-                             .NotEmpty().WithMessage("Debe ingresar la denominacion");
+                             .Matches(@"^[^\d]+$").WithMessage("La razón social no debe contener números")
+                             .NotEmpty().WithMessage("Debe ingresar Razón Social");
+
+            RuleFor(_ => _.Pais).Cascade(CascadeMode.Stop)
+                         .NotEmpty().WithMessage("Debe ingresar País");
+
 
             RuleFor(_ => _.NroFiscal).Cascade(CascadeMode.Stop)
-                             .NotEmpty().WithMessage("Debe ingresar el CUIL O CUIT");
+                .Matches(@"^\d+$").WithMessage("El Cuit no debe contener letras")
+                .NotEmpty().WithMessage("Debe ingresar el CUIL O CUIT").
+                Must(EsCuitValido).WithMessage("El CUIT no es válido");
+
 
             RuleFor(_ => _.Cuotas).Cascade(CascadeMode.Stop)
-                             .NotEmpty().WithMessage("Debe ingresar la cantidad de cuotas");
+                .Matches(@"^\d+$")
+                .WithMessage("La cantidad de cuotas no debe contener letras")
+                .NotEmpty().WithMessage("Debe ingresar la cantidad de cuotas");
 
-            RuleFor(_ => _.Votos).Cascade(CascadeMode.Stop)
-                           .NotEmpty().WithMessage("Debe ingresar la cantidad de votos");
+                             
+
+            RuleFor(_ => _.Votos).Cascade(CascadeMode.Stop).Matches(@"^\d+$")
+                .WithMessage("La cantidad de votos no debe contener letras")
+                .NotEmpty().WithMessage("Debe ingresar la cantidad de votos");
+        }
+
+        public bool EsCuitValido(string cuit)
+        {
+            if (string.IsNullOrEmpty(cuit) || cuit.Length != 11)
+            {
+                return false;
+            }
+
+            int[] multiplicadores = { 5, 4, 3, 2, 7, 6, 5, 4, 3, 2 };
+            int suma = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (!int.TryParse(cuit[i].ToString(), out int digito))
+                {
+                    return false;
+                }
+                suma += digito * multiplicadores[i];
+            }
+
+            int resto = suma % 11;
+            int digitoVerificador = resto == 0 ? 0 : resto == 1 ? 9 : 11 - resto;
+
+            return int.TryParse(cuit[10].ToString(), out int ultimoDigito) && digitoVerificador == ultimoDigito;
         }
     }
 }
