f05567a1b7e1efd1ad05f11b878d91ce031e83f4
Ariel Malerba <arielmalerba84@hotmail.com>
2024-05-28 12:46:51 -0300

    validacion EECC y agregado de notificaciones radzen

--- Balances.Web/Services/FluentValidation/EstadoContableValidator.cs
+++ Balances.Web/Services/FluentValidation/EstadoContableValidator.cs
@@ -28,75 +28,75 @@ namespace Balances.Web.Services.FluentValidation
 
             //RUBROBALANCE
             RuleFor(_ => _.cajaYBancos).Cascade(CascadeMode.Stop)
-                     .NotEmpty().WithMessage("Debe ingresar el monto de caja");
+                     .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el monto de caja");
 
             RuleFor(_ => _.inversionesActivoCorriente).Cascade(CascadeMode.Stop)
-                    .NotEmpty().WithMessage("Debe ingresar el monto de inversiones");
+                   .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el monto de inversiones");
 
             RuleFor(_ => _.activoCorrienteRestante).Cascade(CascadeMode.Stop)
-                   .NotEmpty().WithMessage("Debe ingresar el monto del activo corriente restante");
+                   .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el monto del activo corriente restante");
 
             RuleFor(_ => _.bienesDeUso).Cascade(CascadeMode.Stop)
-               .NotEmpty().WithMessage("Debe ingresar el monto de los bienes de uso");
+               .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el monto de los bienes de uso");
 
 
             RuleFor(_ => _.propiedadesDeInversion).Cascade(CascadeMode.Stop)
-               .NotEmpty().WithMessage("Debe ingresar el monto de las propiedades de inversion");
+              .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el monto de las propiedades de inversion");
 
 
             RuleFor(_ => _.activoNoCorrienteRestante).Cascade(CascadeMode.Stop)
-               .NotEmpty().WithMessage("Debe ingresar el monto del activo no corriente restante");
+               .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el monto del activo no corriente restante");
 
 
             RuleFor(_ => _.activoNoCorriente).Cascade(CascadeMode.Stop)
-               .NotEmpty().WithMessage("Debe ingresar el monto del activo no corriente corriente");
+               .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el monto del activo no corriente corriente");
 
             RuleFor(_ => _.totalActivo).Cascade(CascadeMode.Stop)
-               .NotEmpty().WithMessage("Debe ingresar el monto del total activo");
+              .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el monto del total activo");
 
             RuleFor(_ => _.deudorPasivoCorriente).Cascade(CascadeMode.Stop)
-               .NotEmpty().WithMessage("Debe ingresar el monto de deudas del pasivo corriente");
+               .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el monto de deudas del pasivo corriente");
 
             RuleFor(_ => _.deudorPasivoNoCorriente).Cascade(CascadeMode.Stop)
-               .NotEmpty().WithMessage("Debe ingresar el monto de deudas del pasivo no corriente");
+               .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el monto de deudas del pasivo no corriente");
 
             RuleFor(_ => _.deudorPasivoNoCorriente).Cascade(CascadeMode.Stop)
-            .NotEmpty().WithMessage("Debe ingresar el monto de deudas del pasivo no corriente");
+            .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el monto de deudas del pasivo no corriente");
 
             RuleFor(_ => _.pasivoNoCorriente).Cascade(CascadeMode.Stop)
-            .NotEmpty().WithMessage("Debe ingresar el monto del pasivo no coriente");
+            .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el monto del pasivo no coriente");
             RuleFor(_ => _.totalPasivo).Cascade(CascadeMode.Stop)
-           .NotEmpty().WithMessage("Debe ingresar el monto del total pasivo");
+           .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el monto del total pasivo");
 
             RuleFor(_ => _.totalPasivo).Cascade(CascadeMode.Stop)
-           .NotEmpty().WithMessage("Debe ingresar el monto del patrimonio neto");
+           .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el monto del patrimonio neto");
 
             //RUBROBALANCE
 
             RuleFor(_ => _.capitalSuscripto).Cascade(CascadeMode.Stop)
-           .NotEmpty().WithMessage("Debe ingresar el capital suscripto");
+           .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el capital suscripto");
 
 
             RuleFor(_ => _.ajusteCapital).Cascade(CascadeMode.Stop)
-           .NotEmpty().WithMessage("Debe ingresar el ajuste del capital");
+           .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar el ajuste del capital");
 
             RuleFor(_ => _.aportesIrrevocables).Cascade(CascadeMode.Stop)
-           .NotEmpty().WithMessage("Debe ingresar los aportes irrevocables");
+           .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar los aportes irrevocables");
 
             RuleFor(_ => _.primaEmision).Cascade(CascadeMode.Stop)
-           .NotEmpty().WithMessage("Debe ingresar la prima de emision");
+           .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar la prima de emision");
 
             RuleFor(_ => _.gananciasReservadas).Cascade(CascadeMode.Stop)
-           .NotEmpty().WithMessage("Debe ingresar las ganancias reservadas");
+          .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar las ganancias reservadas");
 
             RuleFor(_ => _.perdidasAcumuladas).Cascade(CascadeMode.Stop)
-           .NotEmpty().WithMessage("Debe ingresar las perdidas acumuladas");
+           .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar las perdidas acumuladas");
 
             RuleFor(_ => _.gananciasPerdidasEjercicio).Cascade(CascadeMode.Stop)
-           .NotEmpty().WithMessage("Debe ingresar las ganancias/perdidas del ejercicio");
+           .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar las ganancias/perdidas del ejercicio");
 
             RuleFor(_ => _.reservaLegal).Cascade(CascadeMode.Stop)
-           .NotEmpty().WithMessage("Debe ingresar la reserva legal");
+           .GreaterThanOrEqualTo(0).WithMessage("Debe ingresar la reserva legal");
 
 
         }
